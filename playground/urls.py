a42a318a6da972bdfbefbb6c1294e920fd03f78f
rohithcodebeat <rohith.baggam@codebeat.in>
2023-06-05 14:52:22 +0530

    Added New APP

--- playground/urls.py
+++ playground/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import test_api, test_json_api, test_html_page
+from .views import test_api, test_json_api, test_html_page, test_hello_name, test_html_for_page, home_page, about_page
 
 urlpatterns = [
     # path( "route-name", views, name="name of the route" )
@@ -7,7 +7,11 @@ urlpatterns = [
     path("test-api/", test_api, name="test_api"),
     path("test-json-api/", test_json_api, name="test_json_api"),
     path("test-html-page/", test_html_page, name="test_html_page"),
-    
+    path("test-html-for-page/", test_html_for_page, name="test_html_for_page"),
+    path("test-hello-name/<name>/", test_hello_name, name="test_hello_name"),
+    path("home-page/", home_page, name="home_page"),
+    path("about-page/", about_page, name="about_page"),
+
 ]
 
 
--- playground/views.py
+++ playground/views.py
@@ -2,11 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
+
+def home_page(request):
+    context = {
+        "page_data" : "Home Page"
+    }
+    return render(request, "playground/raw_page.html", context)
+
+def about_page(request):
+    context = {
+        "page_data" : "About Page"
+    }
+    return render(request, "playground/raw_page.html", context)
+
 # String Message 
 def test_api(request):
     return HttpResponse("First API")
 
-# Json Message
+# Json Message -> Static  API
 def test_json_api(request):
     data = {
         "name" : "First Json API",
@@ -15,13 +28,71 @@ def test_json_api(request):
     }
     return JsonResponse(data)
 
+
 # HTMl Template
 def test_html_page(request):
-    data = {
-        "name" : "First Json API",
-        "framework" : "Python-Django",
-        "university" : "Sit"
+    context = {
+        "name" : "Sit",
+        "framework2024" : "Python-Django",
+        "framework2023" : "Java-Spring-Boot",
+        "year" : 2025   
     }
-    return render(request, "playground/index.html", data)
+    return render(
+        request, 
+        "playground/index.html", 
+        context)
 
 
+# Hello, {{name}} 
+def test_hello_name(request, name):
+    return HttpResponse(f"Hello, {name}")
+
+
+# for loop
+def test_html_for_page(request):
+    context = {
+        "data" : [{
+                "name" : "SIT",
+                "framework" : "Python-Django",
+                "year" : 2023
+            }, {
+                "name" : "SIT",
+                "framework" : "Java-Spring-Boot",
+                "year" : 2024 
+            }]
+        }
+    return render(request, "playground/for_loop.html", context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+def test_list_html_page(request):
+    context = {
+        "data" : [{
+        "name" : "SIT",
+        "framework" : "Python-Django",
+        "year" : 2023
+    }, {
+        "name" : "SIT",
+        "framework" : "Java-Spring-Boot",
+        "year" : 2024 
+    }]
+    }
+    return render(request, "playground/home.html", context)
+
+     {% for d in data %}
+        <h1>{{d.framework}}</h1>
+    {% endfor %}
+"""
